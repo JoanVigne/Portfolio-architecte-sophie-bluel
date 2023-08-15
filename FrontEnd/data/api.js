7f59fcfef4378e0f491c4e91803a0a8b8dbfd612
Joan <joan.vigne@hotmail.fr>
2023-08-15 10:36:05 +0100

    filtre sur buttons categories

--- FrontEnd/data/api.js
+++ FrontEnd/data/api.js
@@ -1,4 +1,4 @@
-export async function getWorks(data) {
+/* export async function getWorks(data) {
   const response = await fetch("http://localhost:5678/api/works");
   return await response.json();
 }
@@ -6,4 +6,24 @@ export async function getWorks(data) {
 export async function getCategories(data) {
   const response = await fetch("http://localhost:5678/api/categories");
   return await response.json();
+} */
+
+async function fetchData(url) {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(
+      `Erreur fetching data de ${url}. Status: ${response.status}`
+    );
+  }
+
+  return response.json();
+}
+
+export async function getWorks() {
+  return await fetchData("http://localhost:5678/api/works");
+}
+
+export async function getCategories() {
+  return await fetchData("http://localhost:5678/api/categories");
 }
---
+++ FrontEnd/js/main.js
@@ -0,0 +1,58 @@
+import { getWorks, getCategories } from "../data/api.js";
+
+let works = [];
+let categories = [];
+
+const init = async () => {
+  works = await getWorks(works);
+  categories = await getCategories(categories);
+  displayWorks(works);
+  displayCategories(categories);
+  console.log(works);
+};
+init();
+
+function displayWorks(works) {
+  let gallery = document.querySelector(".gallery");
+  gallery.innerHTML = "";
+  works.forEach((work) => {
+    gallery.innerHTML += `<figure><img src="${work.imageUrl}" alt="${work.title}">
+      <figcaption>
+       ${work.title} 
+       </figcaption>
+       </figure>`;
+  });
+}
+
+function displayCategories(categories) {
+  let gallery = document.querySelector(".gallery");
+  let categoryContainer = document.createElement("div");
+  categoryContainer.classList.add("categoryContainer");
+  gallery.before(categoryContainer);
+  // creation button "TOUS"
+  let buttonTous = document.createElement("button");
+  buttonTous.innerText = "Tous";
+  buttonTous.addEventListener("click", async () => {
+    works = await getWorks(works);
+    displayWorks(works);
+  });
+  categoryContainer.appendChild(buttonTous);
+  // boucle pour les autres buttons
+  categories.forEach((category) => {
+    let button = document.createElement("button");
+    button.innerHTML = category.name;
+    button.addEventListener("click", (button) => categoryClicked(button));
+    categoryContainer.appendChild(button);
+  });
+}
+
+// onclick sur categories autre que TOUS
+async function categoryClicked(category) {
+  let thisCategory = category.target.innerText;
+  works = await getWorks(works);
+  let onlyThisCategory = works.filter(
+    (work) => work.category.name === thisCategory
+  );
+
+  displayWorks(onlyThisCategory);
+}
