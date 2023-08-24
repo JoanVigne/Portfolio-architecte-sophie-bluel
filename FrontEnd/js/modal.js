bede6702393b047c64969dd95a57da7dcf74034c
Joan <joan.vigne@hotmail.fr>
2023-08-24 13:12:01 +0100

    creation fonction ajouterPhotoModalContent

--- FrontEnd/js/modal.js
+++ FrontEnd/js/modal.js
@@ -1,11 +1,13 @@
 import { deleteWork } from "../js/delete.js";
-import { getWorks } from "../data/api.js";
+import { getWorks, getCategories } from "../data/api.js";
 import { displayWorks } from "./works.js";
 
 let works = [];
+let categories = [];
 
 const modal = document.querySelector(".modal");
 let modalContent = document.querySelector(".modalContent");
+let backModalContent = modalContent.querySelector(".back");
 let h3ModalContent = modalContent.querySelector("h3");
 let buttonModalContent = modalContent.querySelector("button");
 let AModalContent = modalContent.querySelector("a");
@@ -35,7 +37,7 @@ export function modifierProjetModalContent(works) {
       console.log(`Deleting work item : `, work.id);
       areYouSure(work.id);
     });
-    // icone deplacer projet
+    // icone deplacer projet non fonctionnelle POUR LE MOMENT ??????
     const move = document.createElement("span");
     move.textContent = "open_with";
     move.classList.add("material-symbols-outlined", "move");
@@ -44,6 +46,8 @@ export function modifierProjetModalContent(works) {
     gallery.appendChild(figure);
   });
 }
+
+// avant de supprimer un element, confirmation
 export function areYouSure(workId) {
   modal.style.display = null;
   modal.setAttribute("aria-hidden", "false");
@@ -81,6 +85,52 @@ export function modalerreur(textError) {
     modal.style.display = "none";
     modal.setAttribute("aria-hidden", "true");
   });
+}
+
+function ajouterPhotoModalContent() {
+  // creation du form :
+  let formAjouter = document.createElement("form");
+  let apercuPhoto = document.createElement("img");
+  let inputAjouterPhoto = document.createElement("input");
+  let infoPhoto = (document.createElement("p").innerText =
+    "jpg, png : 4mo max");
+  let labelTitre = (document.createElement("label").textContent = "Titre");
+  let inputTitre = document.createElement("input");
+  const getthem = async () => {
+    works = await getWorks(works);
+    getCategories(works);
+  };
+  getthem();
+  let labelCategorie = (document.createElement("label").textContent =
+    "Catégorie");
+  let selectCategorie = document.createElement("select");
+  categories.forEach((categorie) => {
+    let option = document.createElement("option");
+    option.textContent = categorie;
+    option.setAttribute("value", categorie);
+    selectCategorie.append(option);
+  });
+  formAjouter.append(
+    apercuPhoto,
+    inputAjouterPhoto,
+    infoPhoto,
+    labelTitre,
+    inputTitre,
+    labelCategorie,
+    selectCategorie
+  );
+  // insertion du form
+  h3ModalContent.insertAdjacentElement("afterend", formAjouter);
+
+  backModalContent.style.visibility = "visible";
+  backModalContent.addEventListener("click", async () => {
+    works = await getWorks(works);
+    modifierProjetModalContent(works);
+  });
+  h3ModalContent.textContent = "Ajouter photo";
+  gallery.innerHTML = "";
+  buttonModalContent.textContent = "Valider";
+  AModalContent.textContent = "";
 }
 
 export function closeModal() {
